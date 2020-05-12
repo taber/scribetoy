7ee6a102f7e2f70ba03f59a51c1cafda6724ded0
taber <taber@taberbain.com>
2020-05-12 17:48:39 -0400

    much better

--- server.js
+++ server.js
@@ -1,25 +1,16 @@
 // init project
 const express = require("express");
 const bodyParser = require("body-parser");
-const app = express();
-app.use(urlencoded({ extended: true }));
-app.use(json());
 
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 app.use(static("public"));
 
 // init sqlite db
 const dbFile = "./scribetoy.db";
 const sqlite3 = require("sqlite3").verbose();
 const db = new sqlite3.Database(dbFile);
-
-
-db.serialize(() => {
-  if (!exists) {
-    // do create database stuff I guess, not gonna need this here I hope???
-  } else {
-    console.log('refs database is good, here are five refs to prove it!');
-  }
-});
 
 app.get("/", (request, response) => {
   response.sendFile(`${__dirname}/views/index.html`);
